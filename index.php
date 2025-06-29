a2c2e884037c5c015a2ff7dd2cd1c349da73cc40
usmanbosho <aliyuabubakar11117@grmail.com>
2025-06-29 15:10:50 +0100

    2

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title> FMC-BKD | ICT Internship Application portal</title>
+    <title>NACOS - ATBU</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
