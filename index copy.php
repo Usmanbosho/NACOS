c5743fa39364e2a4f744297dc6ee0aa62a841e97
usmanbosho <aliyuabubakar11117@grmail.com>
2025-06-29 15:08:55 +0100

    2

---
+++ index copy.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title> FMC-BKD | ICT Internship Application portal</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link href="img/logo.jpg" rel="icon">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+  	<div class="py-1 bg-black top">
+    	<div class="container">
+    		<div class="row no-gutters d-flex align-items-start align-items-center px-md-0">
+	    		<div class="col-lg-12 d-block">
+		    		<div class="row d-flex">
+		    			<div class="col-md pr-4 d-flex topper align-items-center">
+					    	<div class="icon mr-2 d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
+						    <span class="text">	+2348037439975, +2348036304601</span>
+					    </div>
+					    <div class="col-md pr-4 d-flex topper align-items-center">
+					    	<div class="icon mr-2 d-flex justify-content-center align-items-center"><span class="icon-paper-plane"></span></div>
+						    <span class="text">info@fmcbkd.gov.ng</span>
+					    </div>
+					    <div class="col-md-5 pr-4 d-flex topper align-items-center text-lg-right justify-content-end">
+						    <p class="mb-0 register-link"><a href="login/signup.php" class="mr-3">Sign Up</a><a href="login/">Sign In</a></p>
+					    </div>
+				    </div>
+			    </div>
+		    </div>
+		  </div>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light site-navbar-target" id="ftco-navbar">
+	    <div class="container">
+	      <a class="navbar-brand" href="index.php" ><img src="img/LOGO.jpg" width="70" height="70" style="border-radius: 100px;"> FMC-BKD</a>
+	      <button class="navbar-toggler js-fh5co-nav-toggle fh5co-nav-toggle" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <b><span class="oi oi-menu"></span> </b>
+	      </button>
+
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav nav ml-auto">
+	          <li class="nav-item"><a href="index.php" class="nav-link"><span>Home</span></a></li>
+	          <li class="nav-item"><a href="login/index.php" class="nav-link"><span>Apply</span></a></li>
+	          <li class="nav-item"><a href="login/" class="nav-link"><span>Continue Application</span></a></li>
+	          <li class="nav-item"><a href="admin/" class="nav-link"><span>Administrator</span></a></li>
+            <li class="nav-item"><a href="Admissionlist.php" class="nav-link"><span>Admission List</span></a></li>
+            <li class="nav-item"><a href="https://fmcbkd.gov.ng/contact-2/" class="nav-link"><span>Contact</span></a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+	  
+	  <section class="hero-wrap js-fullheight" style="background-image: url('img/2.jpg'); filter: blur(50);" data-section="home" data-stellar-background-ratio="0.5">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-start" data-scrollax-parent="true">
+          <div class="col-md-6 pt-5 ftco-animate mt-5">
+          	<div class="mt-5">
+          		<span class="subheading"></span>
+	           <b> <h2 class="mb-4 mt-5 mt-5" style="color: black; text-align: center;font-size: 35px;font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;">ICT Unit Skills Builder Academy<br>Portal <script>document.write(new Date().getFullYear());</script><br></h2></b>
+	            <p class="mb-4"></p>
+	            <p><a href="login/" class="btn btn-primary py-3 px-4">Apply</a></p>
+            </div> 
+          </div>
+        </div>
+      </div>
+    </section>    
+     
+
+    <footer class="ftco-footer ftco-section img" style="background-image: url(images/footer-bg.jpg);">
+    	<div class="overlay"></div>
+      <div class="container-fluid px-md-5">
+        <div class="row mb-5">
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">FMC-BKD</h2>
+              <p>Our Social Media Links</p>
+              <ul class="ftco-footer-social list-unstyled mt-5">
+                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+              </ul>
+            </div>
+          </div>
+          <!-- <div class="col-md">
+            <div class="ftco-footer-widget mb-4 ml-md-4">
+              <h2 class="ftco-heading-2">Departments</h2>
+              <ul class="list-unstyled">
+                <li><a href="https://fmcbkd.gov.ng/department/dental-and-maxillifacial/"><span class="icon-long-arrow-right mr-2"></span>Dental and Moxillofacial</a></li>
+                <li><a href="https://fmcbkd.gov.ng/department/ear-nose-and-throat/"><span class="icon-long-arrow-right mr-2"></span> ENT</a></li>
+                <li><a href="https://fmcbkd.gov.ng/department/environment-health/"><span class="icon-long-arrow-right mr-2"></span>Environment Health</a></li>
+                <li><a href="https://fmcbkd.gov.ng/department/health-information-management/"><span class="icon-long-arrow-right mr-2"></span>HIM</a></li>
+                <li><a href="https://fmcbkd.gov.ng/department/medical-laboratory-services/"><span class="icon-long-arrow-right mr-2"></span>Medical Lab science</a></li>
+                <li><a href="https://fmcbkd.gov.ng/department/nursing-services/"><span class="icon-long-arrow-right mr-2"></span>Obsterics & Gynecolocy</a></li>
+                <li><a href="https://fmcbkd.gov.ng/department/obstetrics-gynecology/"><span class="icon-long-arrow-right mr-2"></span>Nursing Services</a></li>
+                <li><a href="https://fmcbkd.gov.ng/department/paediatrics/"><span class="icon-long-arrow-right mr-2"></span>Paediatrics</a></li>
+                <li><a href="https://fmcbkd.gov.ng/department/pharmacy/"><span class="icon-long-arrow-right mr-2"></span>Pharmacy</a></li>
+                <li><a href="https://fmcbkd.gov.ng/department/physiotherapy/"><span class="icon-long-arrow-right mr-2"></span>Physiotheraphy</a></li>
+              </ul>
+            </div>
+          </div> -->
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4 ml-md-4">
+              <h2 class="ftco-heading-2">Links</h2>
+              <ul class="lit-unstyled">
+                <li><a href="index.php"><span class="icon-long-arrow-right mr-2"></span>Home</a></li>
+                <li><a href="login/"><span class="icon-long-arrow-right mr-2"></span>Apply</a></li>
+                <li><a href="login/"><span class="icon-long-arrow-right mr-2"></span>Continue Application</a></li>
+                <li><a href="admin/index.php"><span class="icon-long-arrow-right mr-2"></span>Administrator</a></li>
+                <li><a href="https://fmcbkd.gov.ng/contact-2/"><span class="icon-long-arrow-right mr-2"></span>Contact</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+             <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Classes Offered</h2>
+              <ul class="list-unstyled">
+                <li><a href="introduction_to_computer.php"><span class="icon-long-arrow-right mr-2"></span>Introduction to Computer</a></li>
+                <li><a href="frontend_dev.php"><span class="icon-long-arrow-right mr-2"></span>Frontend Developement</a></li>
+                <li><a href="Officesoftwareessentials.php"><span class="icon-long-arrow-right mr-2"></span>Office Software Essentials</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+            	<h2 class="ftco-heading-2">Have a Questions?</h2>
+            	<div class="block-23 mb-3">
+	              <ul>
+	                <li><span class="icon icon-map-marker"></span><span class="text">Nigeria, jigawa state, Birnin kudu along maiduguri road</span></li>
+	                <li><a href="https://fmcbkd.gov.ng/"><span class="icon icon-phone"></span><span class="text">+2348036316194</span></a></li>
+	                <li><a href="https://fmcbkd.gov.ng/"><span class="icon icon-envelope pr-4"></span><span class="text">info@fmcbkd.gov.ng</span></a></li>
+	              </ul>
+	            </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center"> 
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Internship Portal by <a href="https://fmcbkd.gov.ng" target="_blank"> Information Technology Unit</a>
+</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+     
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
