ae0dcaffd873669df48e9449d0122ea2acd65b16
SupolJungudo <140022917+SupolJungudo@users.noreply.github.com>
2025-07-03 16:13:23 +0200

    Update export_excel.php

--- export_excel.php
+++ export_excel.php
@@ -1,30 +1,15 @@
 <?php
 // export_excel.php
+include('includes/db.php');
 
 header("Content-Type: application/vnd.ms-excel");
 header("Content-Disposition: attachment; filename=students_list.xls");
 
-include("../includes/db.php");
+echo "Name\tReg Number\tCourse\tYear of Admission\tLevel\n";
 
-$output = "<table border='1'>
-<tr>
-  <th>Name</th>
-  <th>Reg Number</th>
-  <th>Course</th>
-  <th>Year of Admission</th>
-  <th>Level</th>
-</tr>";
+$query = "SELECT * FROM students ORDER BY created_at DESC";
+$result = mysqli_query($conn, $query);
 
-$query = mysqli_query($conn, "SELECT * FROM students ORDER BY name ASC");
-while ($row = mysqli_fetch_assoc($query)) {
-  $output .= "<tr>
-    <td>{$row['name']}</td>
-    <td>{$row['reg_number']}</td>
-    <td>{$row['course']}</td>
-    <td>{$row['year_of_admission']}</td>
-    <td>{$row['level']}</td>
-  </tr>";
+while ($row = mysqli_fetch_assoc($result)) {
+    echo $row['name'] . "\t" . $row['reg_number'] . "\t" . $row['course'] . "\t" . $row['admission_year'] . "\t" . $row['level'] . "\n";
 }
-
-$output .= "</table>";
-echo $output;
